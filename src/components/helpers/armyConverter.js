2192b937b83a6934060bf674374989c182294700
Matthew <matthew.tudman@gmail.com>
2020-08-05 18:24:09 +1000

    army convertor up and running

--- src/components/helpers/armyConverter.js
+++ src/components/helpers/armyConverter.js
@@ -6,15 +6,6 @@ const input = fs.readFileSync('../test/DarkAngelsCurrent.html', 'utf-8')
 const fileName = './DarkAngelsCurrent.json'
 
 
-
-fs.writeFile(
-  fileName,
-  input,
-  'utf-8',
-  () => {
-    console.log(trimJSON(fs.readFileSync(`${fileName}`, 'utf-8')));
-  }
-)
 // Take the JSON file and then
 const trimJSON = (inputJSON) => {
   const armies = [
@@ -40,26 +31,34 @@ const trimJSON = (inputJSON) => {
   }
 
   const createArrayofDetachments = (inputJSON) => {
-    return {detachments: [$('.force', inputJSON).map((i, det) => {
-      detachmentName: $('h2', det).map((i, title) => $(title).text()).get()
-      })]
+    return {
+      armyName: $('h1', inputJSON).text(),
+      detachments: $('.force', inputJSON).map((i, det) => {
+        return getDetachmentName(det)
+      }).get()}
+  }
+
+
+  const getDetachmentName = (det) => {
+    return {
+      detachmentName: $('h2', det).text(),
+      battlefieldRoles: createBattlefieldRoles(det)
     }
   }
 
-  // const createArrayofDetachments = (inputJSON) => {
-  //   const detachmentArray = $('.force', inputJSON)
-  //   return { detachments:
-  //     detachmentArray.map((i, det) => {
-  //       detachmentName: detTitles(det),
-  //       battlefieldRoles: batRoles(det)
-  //     }).get();
-  //   }
-  // }
+  const createBattlefieldRoles = (detInput) => {
+    return $('.category', detInput).map((i, roles) => {
+      return getBattlefieldRoleName(roles)
+    }).get()
+  }
 
-  // const detachments = $('.force', inputJSON);
-  // const detachmentArray = $('.force', inputJSON).map((i, det) =>
-  //   detTitles(det) + " " + batRoles(det)
-  // ).get()
+  const getBattlefieldRoleName = (rolesInput) => {
+    return {
+      roleName: $('h3', rolesInput).text(),
+      units: getUnits(rolesInput)
+    }
+  }
+
 
   // const batRoles = (detInput) => {
   //   return $('.category', detInput).map((i, r) => {
@@ -68,7 +67,7 @@ const trimJSON = (inputJSON) => {
   // }
 
   const getUnits = (roleInput) => {
-    return $  ('.rootselection > h4', roleInput).map((i, u) => $(u).text()).get();
+    return $('.rootselection > h4', roleInput).map((i, u) => $(u).text()).get();
   }
 
   const detTitles = (det) => {
@@ -81,25 +80,13 @@ const trimJSON = (inputJSON) => {
   // get() changes it back to an array
 
   // armies.push(getArmyName(inputJSON))
-  console.log(createArrayofDetachments(inputJSON));
-
-  // return armies;
+  return JSON.stringify(createArrayofDetachments(inputJSON), null, 4)
 }
 
 
-const out = [
-  {
-    armyName: "",
-    detachments: [
-      {
-        detachmentName: "",
-        battlefieldRoles: [
-          {
-            roleName: "",
-            units: [""]
-          }
-        ]
-      }
-    ]
-  }
-]
+fs.writeFile(
+  fileName,
+  trimJSON(input),
+  'utf-8',
+  () => {}
+)
